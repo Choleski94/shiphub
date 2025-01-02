eb347d0612345f12c7efec223c2effc8f62a12c1
Choleski Louis <louischoleski@gmail.com>
2025-01-02 02:34:16 -0500

    Add support for selectToken selector.

--- src/store/selectors/app.ts
+++ src/store/selectors/app.ts
@@ -3,3 +3,5 @@ import { createSelector } from "reselect";
 const appSelect = (state) => state.app;
 
 export const selectApp = createSelector([appSelect], (app) => app);
+
+export const selectToken = createSelector([appSelect], (app) => app.token);
