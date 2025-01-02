11df3210c58495b03773e40815b3dcda87ce3f68
Choleski Louis <louischoleski@gmail.com>
2025-01-02 14:11:41 -0500

    Update profile screen email section.

--- src/screens/ProfileScreen/sections/Email/Email.tsx
+++ src/screens/ProfileScreen/sections/Email/Email.tsx
@@ -1,43 +1,57 @@
 import React from 'react';
 import { View } from 'react-native';
-import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import { useForm, Controller } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
 
+import useApi from '../../../../api';
 import styles from './../../ProfileScreen.styled';
 import { selectUser } from '../../../../store/selectors/user';
 import { Card, Button, Grid, Input } from '../../../../components';
 
 const ProfileEmailSection = ({ onSectionPress }) => {
+    const api = useApi();
+    const dispatch = useDispatch();
     const { t } = useTranslation();
     const userData = useSelector(selectUser);
 
-    const [data, setData] = React.useState({
-        email: '',
-        oldEmail: '',
+    const {
+        control,
+        setValue,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm({
+        defaultValues: {
+            email: '',
+            oldEmail: '',
+        },
+        mode: 'all',
     });
 
-    console.log('--->', userData)
+    const errorMessages = {
+        empty: t('form.validation.empty.error.text'),
+        email: t('form.validation.email.error.text'),
+    };
 
     React.useEffect(() => {
-        setData({
-            ...data,
-            oldEmail: userData.email
-        });
-    }, [userData]);
+        if (userData?.email && userData?.email.length) {
+            setValue('oldEmail', userData.email); // Set oldEmail value
+        }
+    }, [userData, setValue]);
 
-    const onChangeValue = (value: string, key?: string) => {
-        if (!key || !key.length) return;
+    const onSubmit = (data: any) => {
+        // api.user.login(data)
+        //     .then((user) => {
+        //         dispatch(logInUser(user));
+        //     })
+        //     .catch((error) => {
+        //         console.error('Login error:', error);
+        //     });
+    };
 
-        setData({ ...data, [key]: value });
-    }
+    const onSavePressed = () => handleSubmit(onSubmit)();
 
-    const onSavePressed = () => {
-        onSectionPress();
-    }
-
-    const onExitPressed = () => {
-        onSectionPress();
-    }
+    const onExitPressed = () => onSectionPress();
 
     return (
         <>
@@ -46,23 +60,67 @@ const ProfileEmailSection = ({ onSectionPress }) => {
                 description={t('screen.profile-email.paragraph.text')}
             >
                 <Grid cols={1}>
-                    <Input
-                        disabled
-                        type="EMAIL"
+                    <Controller
                         name="oldEmail"
-                        value={data.oldEmail}
-                        setValue={onChangeValue}
-                        label={t('screen.profile-email.form.email.text')}
+                        control={control}
+                        rules={{
+                            required: errorMessages.empty,
+                            validate: (value) => {
+                                // First, check if the email is empty, then check for the format
+                                if (!value) {
+                                    return errorMessages.empty;
+                                }
+                                // if (!validateEmail(value)) {
+                                //     return t('form.validation.email.error.text'); // Custom error message for invalid email
+                                // }
+
+                                return true; // If email is valid, return true
+                            }
+                        }}
+                        render={({ field }) => (
+                            <Input
+                                disabled
+                                type="EMAIL"
+                                name="oldEmail"
+                                value={field.value}
+                                onBlur={field.onBlur}
+                                setValue={field.onChange}
+                                error={errors.oldEmail?.message}
+                                label={t('screen.profile-email.form.email.text')}
+                            />
+                        )}
                     />
                 </Grid>
 
                 <Grid cols={1}>
-                    <Input
-                        type="EMAIL"
+                    <Controller
                         name="email"
-                        value={data.email}
-                        setValue={onChangeValue}
-                        label={t('screen.profile-email.form.email-confirm.text')}
+                        control={control}
+                        rules={{
+                            required: errorMessages.empty,
+                            validate: (value) => {
+                                // First, check if the email is empty, then check for the format
+                                if (!value) {
+                                    return errorMessages.empty;
+                                }
+                                // if (!validateEmail(value)) {
+                                //     return t('form.validation.email.error.text'); // Custom error message for invalid email
+                                // }
+
+                                return true; // If email is valid, return true
+                            }
+                        }}
+                        render={({ field }) => (
+                            <Input
+                                type="EMAIL"
+                                name="email"
+                                value={field.value}
+                                onBlur={field.onBlur}
+                                setValue={field.onChange}
+                                error={errors.email?.message}
+                                label={t('screen.profile-email.form.email-confirm.text')}
+                            />
+                        )}
                     />
                 </Grid>
             </Card>
@@ -70,6 +128,7 @@ const ProfileEmailSection = ({ onSectionPress }) => {
             <View style={styles.logout}>
                 <Button
                     type="PRIMARY"
+                    loading={isSubmitting}
                     onPress={onSavePressed}
                     text={t('screen.profile-main.save.text')}
                 />
